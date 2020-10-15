12f442e5c1da8a41e2930650320a752820acb5c3
26kevin1997 <71206451+26kevin1997@users.noreply.github.com>
2020-10-15 13:06:35 -0500

    Add files via upload

--- main.go
+++ main.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+
+	var s []int
+	var x int = 0
+	var n int
+	var a int
+	fmt.Println("Cuantos numeros quiere agregar: ")
+	fmt.Scan(&n)
+
+	for i := 0; i < n; i++ {
+		fmt.Println("Numero: ")
+		fmt.Scan(&a)
+		s = append(s, a)
+		x = x + s[i]
+
+	}
+	fmt.Println("Resultado: ", x)
+
 }
